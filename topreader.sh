74e2f22d882fd3a1be5d88a52eb19a0ac98a1d10
Jeff Y <17779519+yjst2012@users.noreply.github.com>
2021-09-22 16:23:13 +1200

    initial commit

---
+++ topreader.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+state="true"
+
+while $state
+do
+    SMALL_REQUESTS=$(netstat -ant | awk -F'[ :]+' '/:22/{count[$4]++} END {for(ip in count) print count[ip]}' | sort -n | head -20 | head -1)
+    if [ "$SMALL_REQUESTS" -gt 500 ];then
+        sar -A > alert.txt
+        state="false"
+    else
+        sleep 6
+        continue
+    fi
+done    
